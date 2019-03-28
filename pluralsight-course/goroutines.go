f76dff722aea437d46b505da50eab4b060326983
Josh Ghent <josh.ghent@cloudcall.com>
2019-03-28 14:30:18 +0000

    Added channels to goroutines example

--- pluralsight-course/goroutines.go
+++ pluralsight-course/goroutines.go
@@ -2,20 +2,37 @@ package main
 
 import (
 	"runtime"
-	"time"
 )
 
 func main() {
 	runtime.GOMAXPROCS(4)
-	go abc() // run as a goroutine, we need to sleep the thread or otherwise the app will quit before the method has been called
 
-	println("this comes first")
+	ch := make(chan string)
+	doneCh := make(chan bool) // channel for when it is done
+	go abc(ch)                // run as a goroutine, we need to sleep the thread or otherwise the app will quit before the method has been called
+	go printer(ch, doneCh)
 
-	time.Sleep(100 * time.Millisecond)
+	// Receive the message on the done channel
+	// Main will stay alive until this completes
+	<-doneCh
 }
 
-func abc() {
+func abc(ch chan string) {
 	for l := byte('a'); l <= byte('z'); l++ {
-		go println(string(l))
+		// Pass the string to the channel
+		ch <- string(l)
 	}
+
+	// Close the channel so it will be no longer available
+	close(ch)
+}
+
+func printer(ch chan string, doneCh chan bool) {
+	// Wait for messages to come in on the channel
+	for l := range ch {
+		println(l)
+	}
+
+	// Pass true message on the done channel
+	doneCh <- true
 }
